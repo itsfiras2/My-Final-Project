1b195c0dddf5523ca0c2c96d10cba17c9ac27a1f
Firas <77270818+itsfiras2@users.noreply.github.com>
2021-04-15 11:14:21 +0100

    Add files via upload

---
+++ client/src/components/Navbar/styles.js
@@ -0,0 +1,48 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { deepPurple } from '@material-ui/core/colors';
+
+export default makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 5 ,
+
+    margin: '30px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '10px 50px',
+  },
+  heading: 
+  {
+  "color":"#a3f7bf ",
+  "marginLeft":"0px",
+  "position":"relative",
+  "top":"-4px",
+  "fontSize":"40px"},
+  image: {
+    marginLeft: '20px',
+    marginRight: '20px',
+  },
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    width: '400px',
+  },
+  profile: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '400px',
+  },
+  userName: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  brandContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  purple: {
+    color: theme.palette.getContrastText(deepPurple[700]),
+    backgroundColor: deepPurple[500],
+  },
+}));
